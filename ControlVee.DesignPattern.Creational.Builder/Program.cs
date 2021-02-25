50cbf2153e0d6c4bc571b55e4602afcd0468d736
controlvee <adam.goerlich@gmail.com>
2021-02-24 21:14:06 -0600

    .

--- ControlVee.DesignPattern.Creational.Builder/Program.cs
+++ ControlVee.DesignPattern.Creational.Builder/Program.cs
@@ -4,28 +4,31 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /*  
-         Participants:
-        
-            The classes and objects participating in this pattern are:
-                Builder  (VehicleBuilder)
-                    - specifies an abstract interface for creating parts of a Product object
-                    - (optional) implements the successor link
+    /* 
+        Summary:  Separate the construction of a complex object from its representation 
+        so that the same construction process can create different representations.
 
-                ConcreteBuilder  (MotorCycleBuilder, CarBuilder, ScooterBuilder)
-                    - constructs and assembles parts of the product by implementing the Builder interface
-                    - defines and keeps track of the representation it creates
-                    - provides an interface for retrieving the product
+        Participants -
+        The classes and objects participating in this pattern are:
 
-                Director  (Shop)
-                    - constructs an object using the Builder interface
+            Builder  (VehicleBuilder)
+                - specifies an abstract interface for creating parts of a Product object
+                - (optional) implements the successor link
 
-                Product  (Vehicle)
-                    - represents the complex object under construction. 
-                    ConcreteBuilder builds the product's internal representation 
-                    and defines the process by which it's assembled
-                    - includes classes that define the constituent parts, including 
-                    interfaces for assembling the parts into the final result
+            ConcreteBuilder  (MotorCycleBuilder, CarBuilder, ScooterBuilder)
+                - constructs and assembles parts of the product by implementing the Builder interface
+                - defines and keeps track of the representation it creates
+                - provides an interface for retrieving the product
+
+            Director  (Shop)
+                - constructs an object using the Builder interface
+
+            Product  (Vehicle)
+                - represents the complex object under construction. 
+                ConcreteBuilder builds the product's internal representation 
+                and defines the process by which it's assembled
+                - includes classes that define the constituent parts, including 
+                interfaces for assembling the parts into the final result
     */
     /// </summary>
     class Program
